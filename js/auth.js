b80566cf418cc2d6c401d44529a3a1fe26a789ab
naysantana98 <naysantana98@icloud.com>
2026-09-09 15:19:04 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -6,25 +6,44 @@ const message = document.getElementById("message");
 
 if (loginForm) loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+
   message.textContent = "Entrando...";
+
   const { error } = await supabaseClient.auth.signInWithPassword({
     email: document.getElementById("email").value,
     password: document.getElementById("password").value
   });
-  if (error) return message.textContent = error.message;
+
+  if (error) {
+    message.textContent = error.message;
+    return;
+  }
+
   location.href = "aluno.html";
 });
 
 if (registerForm) registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
+
   message.textContent = "Criando conta...";
+
   const { data, error } = await supabaseClient.auth.signUp({
-  email: document.getElementById("email").value,
-  password: document.getElementById("password").value,
-  options: {
-    data: {
-      full_name: document.getElementById("name").value
-    },
-    emailRedirectTo: "https://naysantana98.github.io/plataforma-cursos/login.html"
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+    options: {
+      data: {
+        full_name: document.getElementById("name").value
+      },
+      emailRedirectTo: "https://naysantana98.github.io/plataforma-cursos/login.html"
+    }
+  });
+
+  if (error) {
+    message.textContent = error.message;
+    return;
   }
+
+  message.textContent = data.session
+    ? "Conta criada."
+    : "Conta criada. Confira seu e-mail para confirmar o cadastro.";
 });
