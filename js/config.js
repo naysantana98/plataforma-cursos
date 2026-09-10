1bb9bb90b4178b2f9e82c15ad2bac3904d8831d3
naysantana98 <naysantana98@icloud.com>
2026-09-10 09:07:06 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // PREENCHA antes de publicar.
 // Use somente a chave pública/anon do Supabase neste arquivo.
-const SUPABASE_URL = https://vfbwdcdzfuupdsnlxhds.supabase.co";
+const SUPABASE_URL = "https://vfbwdcdzfuupdsnlxhds.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_gLPkzPZESSmuY6TNneeWSA_VIXDZvTR";
